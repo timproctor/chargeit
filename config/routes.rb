da98a4d63a3cd3c7ee77ad01a2ef6dfb2d7dacce
timproctor <timandcamiproctor@gmail.com>
2014-10-14 20:02:00 -0600

    added destroy and create in sessions controller, created a sign-in through omniauth, created a sign-out, created views for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'search', to: 'charging_stations#index'
   get 'map',     to: 'charging_stations#map'
 
-  match "/auth/twitter/callback" => "sessions#create"
+  match "/auth/twitter/callback", to: "sessions#create", via: [:get, :post]
+  match "/signout", to: "sessions#destroy", as: :signout
 end
